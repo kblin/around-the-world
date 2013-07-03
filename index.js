9ed024f8bbac9968b34bf4afd4bd0e1b9f2a0e07
Kai Blin <kai@samba.org>
2013-07-03 09:07:47 +0200

    Add basic server infrastructure
    Signed-off-by: Kai Blin <kai@samba.org>

---
+++ index.js
@@ -0,0 +1,5 @@
+var hw = require('./hardware'),
+    server = require('./server');
+
+hw.init();
+server.start();
---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require("http"),
+    url = require("url");
+
+function start() {
+    function onRequest(request, response) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello World!');
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888);
+    console.log('server started');
+}
+
+exports.start = start;
